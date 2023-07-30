0549c3537ecf55a04d7f881bfecfa2635e83d504
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-07-31 00:38:22 +0900

    Default Application

--- src/main/java/org/knulikelion/moneyisinvest/MoneyIsInvestApplication.java
+++ src/main/java/org/knulikelion/moneyisinvest/MoneyIsInvestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringInitApplication {
+public class MoneyIsInvestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringInitApplication.class, args);
+        SpringApplication.run(MoneyIsInvestApplication.class, args);
     }
 
 }
