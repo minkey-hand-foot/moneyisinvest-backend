8550c3b0d50241bb9bf4f6b3c0ff9ad22076a5d0
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-07-31 00:25:22 +0900

    setting: Initialize Project

---
+++ src/test/java/org/knulikelion/moneyisinvest/SpringInitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.knulikelion.moneyisinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
