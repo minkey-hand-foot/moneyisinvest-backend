550a57837b2b7d11b12a0977e7d7a226ee4920f5
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-07-31 00:28:29 +0900

    setting: Initialize Project

---
+++ src/main/java/org/knulikelion/moneyisinvest/SpringInitApplication.java
@@ -0,0 +1,13 @@
+package org.knulikelion.moneyisinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitApplication.class, args);
+    }
+
+}
