9c17dd86f4d3ece857f1133865bf528cef56a4ba
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-08 04:04:35 +0900

    feat : StockWebSocketService 추가
    StockWebSocketService interface 추가 하였습니다.

---
+++ src/main/java/org/knulikelion/moneyisinvest/service/StockWebSocketService.java
@@ -0,0 +1,17 @@
+package org.knulikelion.moneyisinvest.service;
+
+import org.knulikelion.moneyisinvest.data.dto.response.KosdaqResponseDto;
+import org.knulikelion.moneyisinvest.data.dto.response.KospiResponseDto;
+import org.knulikelion.moneyisinvest.data.dto.response.StockPriceResponseDto;
+import org.knulikelion.moneyisinvest.data.dto.response.StockRankResponseDto;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface StockWebSocketService {
+    StockPriceResponseDto getStock(String stockCode) throws IOException, JSONException;
+    List<StockRankResponseDto> getStockRank() throws IOException, JSONException;
+    KospiResponseDto getKospi() throws IOException;
+    KosdaqResponseDto getKosdaq() throws IOException;
+}
