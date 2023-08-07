95e6abe98dd51daa3f9e45a3b3e7e57a84e3900e
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-08 04:02:28 +0900

    feat : Scheduling Annotation 추가
    WebSocket Handling 시 사용할 Scheduling 관련 Annotation 추가하였습니다.

--- src/main/java/org/knulikelion/moneyisinvest/MoneyIsInvestApplication.java
+++ src/main/java/org/knulikelion/moneyisinvest/MoneyIsInvestApplication.java
@@ -2,8 +2,10 @@ package org.knulikelion.moneyisinvest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MoneyIsInvestApplication {
 
     public static void main(String[] args) {
