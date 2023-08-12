5da6844bda0f616f2d7f2e66c872604bd5bb5ee4
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-08-13 02:07:49 +0900

    feat: 스톡 코인을 이용하여 상품 구매

--- src/main/java/org/knulikelion/moneyisinvest/controller/ShopController.java
+++ src/main/java/org/knulikelion/moneyisinvest/controller/ShopController.java
@@ -1,12 +1,10 @@
 package org.knulikelion.moneyisinvest.controller;
 
+import org.knulikelion.moneyisinvest.data.dto.response.BaseResponseDto;
 import org.knulikelion.moneyisinvest.data.entity.Shop;
 import org.knulikelion.moneyisinvest.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,5 +27,10 @@ public class ShopController {
     @GetMapping("/get/items/id")
     public Optional<Shop> getItemsById(@RequestParam Long id) {
         return shopService.getItemsById(id);
+    }
+
+    @PostMapping("/buy/items/id")
+    public BaseResponseDto buyItemsById(@RequestParam Long id, String username) {
+        return shopService.buyItemsById(id, username);
     }
 }
--- src/main/java/org/knulikelion/moneyisinvest/service/ShopService.java
+++ src/main/java/org/knulikelion/moneyisinvest/service/ShopService.java
@@ -1,5 +1,6 @@
 package org.knulikelion.moneyisinvest.service;
 
+import org.knulikelion.moneyisinvest.data.dto.response.BaseResponseDto;
 import org.knulikelion.moneyisinvest.data.entity.Shop;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface ShopService {
     List<Shop> getAllItems();
     Optional<Shop> getItemsById(Long id);
+    BaseResponseDto buyItemsById(Long id, String username);
 }
