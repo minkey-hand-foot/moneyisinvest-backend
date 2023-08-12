852d78e73ef92e31e9149796c9d1da9a65443d5a
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-08-13 01:40:18 +0900

    feat: 특정 상점 상품 정보 조회 기능 추가

--- src/main/java/org/knulikelion/moneyisinvest/controller/ShopController.java
+++ src/main/java/org/knulikelion/moneyisinvest/controller/ShopController.java
@@ -5,9 +5,11 @@ import org.knulikelion.moneyisinvest.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/shop")
@@ -22,5 +24,10 @@ public class ShopController {
     @GetMapping("/get/items")
     public List<Shop> getAllItems() {
         return shopService.getAllItems();
+    }
+
+    @GetMapping("/get/items/id")
+    public Optional<Shop> getItemsById(@RequestParam Long id) {
+        return shopService.getItemsById(id);
     }
 }
--- src/main/java/org/knulikelion/moneyisinvest/service/ShopService.java
+++ src/main/java/org/knulikelion/moneyisinvest/service/ShopService.java
@@ -3,7 +3,9 @@ package org.knulikelion.moneyisinvest.service;
 import org.knulikelion.moneyisinvest.data.entity.Shop;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ShopService {
     List<Shop> getAllItems();
+    Optional<Shop> getItemsById(Long id);
 }
