6385344d94e462422f995f27114a02c4c5f6de06
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-13 20:49:45 +0900

    feat : Stock Entity 추가

---
+++ src/main/java/org/knulikelion/moneyisinvest/data/entity/Stock.java
@@ -0,0 +1,37 @@
+package org.knulikelion.moneyisinvest.data.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String stockCode;
+
+    @Column
+    private String stockAmount;
+
+    @Column
+    private Integer conclusion_price;
+
+    @Column
+    private Integer conclusion_coin;
+
+    @Column
+    private Double rate;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
