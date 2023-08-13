b6345157b10c1416033a0671e4442daff02c9513
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-13 20:50:54 +0900

    feat : StockService Interface 추가

--- src/main/java/org/knulikelion/moneyisinvest/service/StockService.java
+++ src/main/java/org/knulikelion/moneyisinvest/service/StockService.java
@@ -1,6 +1,10 @@
 package org.knulikelion.moneyisinvest.service;
 
+import org.knulikelion.moneyisinvest.data.dto.request.StockBuyRequestDto;
 import org.knulikelion.moneyisinvest.data.dto.response.*;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -12,4 +16,12 @@ public interface StockService {
     CheckHolidayResponseDto checkIsHolidayNow();
     List<HolidayResponseDto> getAllHoliday();
     StockCompanyFavResponseDto getCompanyFavByStockId(String stockId);
+
+    /**
+     * @param stockBuyRequestDto
+     * stockCode, stockAmount, conclusion_price, date
+     * @return BaseResponseDto
+     * successs, msg
+     */
+    BaseResponseDto buyStock(StockBuyRequestDto stockBuyRequestDto) throws JSONException, IOException;
 }
