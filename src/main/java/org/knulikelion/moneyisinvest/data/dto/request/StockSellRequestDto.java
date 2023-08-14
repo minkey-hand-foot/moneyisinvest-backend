b95665818a62c2b17d3b8b8fd4c63f1260a24f2a
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-14 17:46:20 +0900

    feat : StockSellRequestDto 추가

---
+++ src/main/java/org/knulikelion/moneyisinvest/data/dto/request/StockSellRequestDto.java
@@ -0,0 +1,15 @@
+package org.knulikelion.moneyisinvest.data.dto.request;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class StockSellRequestDto {
+    private String uid;
+    private String stockCode;
+    private String stockAmount;
+    private String sell_price;
+}
