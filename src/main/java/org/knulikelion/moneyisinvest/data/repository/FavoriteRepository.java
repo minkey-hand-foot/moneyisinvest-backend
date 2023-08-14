306c7cf9ebd90056f61d5c2e2545a475715fe32d
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-14 10:58:11 +0900

    fix: FavoriteRepository 불필요한 코드 삭제.

--- src/main/java/org/knulikelion/moneyisinvest/data/repository/FavoriteRepository.java
+++ src/main/java/org/knulikelion/moneyisinvest/data/repository/FavoriteRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findAllByStockId(String stockId);
     List<Favorite> findByUserAndStockId(User user, String stockId);
-    List<Favorite> findByUserId(String userId);
-    Favorite findByStockId(String stockId);
+    List<Favorite> findByUser(User user);
 }
