c23e123d8b9d29b5b601e17efcccf941b71f5142
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-14 11:16:29 +0900

    fix: favorite, support 권한 설정 및 주석 수정.

--- src/main/java/org/knulikelion/moneyisinvest/config/security/SecurityConfiguration.java
+++ src/main/java/org/knulikelion/moneyisinvest/config/security/SecurityConfiguration.java
@@ -79,7 +79,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity webSecurity) {
-        webSecurity.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**",
-                "/swagger-ui.html", "/webjars/**", "/swagger/**", "/sign-api/exception");
+        webSecurity.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**");
     }
 }
