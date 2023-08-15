6b0de389e4baf40bf3deafca00c90753ef22b512
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-16 02:39:07 +0900

    feat: StockTransactionDto 추가.

---
+++ src/main/java/org/knulikelion/moneyisinvest/data/dto/response/StockTransactionResponseDto.java
@@ -0,0 +1,31 @@
+package org.knulikelion.moneyisinvest.data.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.knulikelion.moneyisinvest.data.entity.StockTransaction;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class StockTransactionResponseDto {
+
+    private String transactionType; // 거래 종류(구매/판매)
+    private String stockCode; // 주식 코드
+    private String stockName; // 주식 이름
+    private int quantity; // 거래 수량
+    private double unitPrice; // 단가
+    private LocalDateTime transactionDate; // 거래 일시
+
+    public StockTransactionResponseDto(StockTransaction transaction) {
+        this.transactionType = transaction.isPurchase() ? "구매" : "판매";
+        this.stockCode = transaction.getStockCode();
+        this.stockName = transaction.getStockName();
+        this.quantity = transaction.getQuantity();
+        this.unitPrice = transaction.getUnitPrice();
+        this.transactionDate = transaction.getTransactionDate();
+    }
+
+}
