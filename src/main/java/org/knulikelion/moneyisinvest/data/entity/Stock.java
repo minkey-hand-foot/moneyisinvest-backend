da57a7310d185e874dc607435bae536074e87b39
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-16 02:37:00 +0900

    fix : Stock Entity 필드 수정

--- src/main/java/org/knulikelion/moneyisinvest/data/entity/Stock.java
+++ src/main/java/org/knulikelion/moneyisinvest/data/entity/Stock.java
@@ -17,25 +17,46 @@ public class Stock {
     private Long id;
 
     @Column
-    private String stockCode;
+    private String stockUrl; // 로고
 
     @Column
-    private String stockUrl;
+    private String stockCode; // 종목코드
 
     @Column
-    private boolean favorite_status;
+    private String stockName; // 종목 이름
 
     @Column
-    private String stockAmount;
+    private Double rate; // 수익률
 
     @Column
-    private Integer conclusion_price;
+    private Integer stockAmount; // 총 보유 수량
 
     @Column
-    private Integer conclusion_coin;
+    private Integer real_sum_coin_price; // 실제 종목의 평가 총 코인 가격
 
     @Column
-    private Double rate;
+    private Integer real_sum_price; // 실제 종목의 평가 총 가격
+
+    @Column
+    private Integer my_conclusion_sum_coin; // 내 체결 총 코인 가격
+
+    @Column
+    private Integer my_conclusion_sum_price; // 내 체결 총 가격
+
+    @Column
+    private Integer my_per_conclusion_coin; // 보유 평 코인
+
+    @Column
+    private Integer my_per_conclusion_price; // 평 단가
+
+    @Column
+    private Integer real_per_coin; // 실제 평 코인
+
+    @Column
+    private Integer real_per_price; // 실제 평 단가
+
+    @Column
+    private boolean favorite_status; // 찜 여부
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
