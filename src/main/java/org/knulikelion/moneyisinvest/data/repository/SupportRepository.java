8cba30e72be9a9cf9e0e344a0dfb7e8c2c05e82a
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-17 05:01:31 +0900

    fix : SupportRepository 수정

--- src/main/java/org/knulikelion/moneyisinvest/data/repository/SupportRepository.java
+++ src/main/java/org/knulikelion/moneyisinvest/data/repository/SupportRepository.java
@@ -1,6 +1,7 @@
 package org.knulikelion.moneyisinvest.data.repository;
 
 import org.knulikelion.moneyisinvest.data.entity.Support;
+import org.knulikelion.moneyisinvest.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +12,6 @@ import java.util.Optional;
 public interface SupportRepository extends JpaRepository<Support, Long> {
     void delete (Support getSupport);
     List<Support> findAllByUserUid(String uid);
-
     Optional<Support> findByIdAndUserUid(Long supportId, String uid);
-
-    /*@Transactional
-    void deleteByIdAndUserId(Long supportId, Long userId);*/
+    List<Support> findByUser_Id(Long userId);
 }
