c4d412f4a6fdffe6bd9661c55ad60bd0307c519a
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-16 20:32:04 +0900

    fix : FavoriteController param 값 수정

--- src/main/java/org/knulikelion/moneyisinvest/controller/FavoriteController.java
+++ src/main/java/org/knulikelion/moneyisinvest/controller/FavoriteController.java
@@ -3,20 +3,17 @@ package org.knulikelion.moneyisinvest.controller;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
-import org.knulikelion.moneyisinvest.data.dto.request.FavoriteRequestDto;
+import org.knulikelion.moneyisinvest.config.security.JwtTokenProvider;
 import org.knulikelion.moneyisinvest.data.dto.response.BaseResponseDto;
 import org.knulikelion.moneyisinvest.data.dto.response.StockCompanyFavResponseDto;
-import org.knulikelion.moneyisinvest.data.dto.response.StockCompanyInfoResponseDto;
 import org.knulikelion.moneyisinvest.service.FavoriteService;
 import org.knulikelion.moneyisinvest.service.StockService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletRequest;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,34 +22,30 @@ public class FavoriteController {
 
     private final FavoriteService favoriteService;
     private final StockService stockService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     // 관심 주식 추가
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @PostMapping("/post")
-    public BaseResponseDto addFavorite(@RequestParam("uid") String uid, @RequestParam("stockId") String stockId) {
-        return favoriteService.addFavorite(uid,stockId);
+    public BaseResponseDto addFavorite(HttpServletRequest request, @RequestParam("stockId") String stockId) {
+        return favoriteService.addFavorite(jwtTokenProvider.getUsername(request.getHeader("X-AUTH-TOKEN")), stockId);
     }
     // 관심 주식 삭제
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @DeleteMapping("/remove")
-    public BaseResponseDto removeFavorite(@RequestParam("uid") String uid, @RequestParam("stockId") String stockId) {
-        return favoriteService.removeFavorite(uid,stockId);
+    public BaseResponseDto removeFavorite(HttpServletRequest request, @RequestParam("stockId") String stockId) {
+        return favoriteService.removeFavorite(jwtTokenProvider.getUsername(request.getHeader("X-AUTH-TOKEN")),stockId);
     }
     // 관심 주식 리스트 끌어오기
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @GetMapping("/get")
-    public ResponseEntity<List<StockCompanyFavResponseDto>> getUserFavoriteStock(@RequestParam("uid") String uid) {
-        List<String> favoriteStockIds = favoriteService.findUserFavoriteStockIds(uid);
-        List<StockCompanyFavResponseDto> favoriteStocks = favoriteStockIds.stream()
-                .map(stockService::getCompanyFavByStockId)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(favoriteStocks);
+    public List<StockCompanyFavResponseDto> getUserFavoriteStock(HttpServletRequest request) {
+        return favoriteService.findUserFavoriteStockIds(jwtTokenProvider.getUsername(request.getHeader("X-AUTH-TOKEN")));
     }
 }
