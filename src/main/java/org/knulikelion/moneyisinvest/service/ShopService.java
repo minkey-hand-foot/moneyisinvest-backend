586f004bc6dec8206602386df0afa81e894b0ef9
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-08-18 03:15:45 +0900

    feat: 상점 페이징, 소수점 처리

--- src/main/java/org/knulikelion/moneyisinvest/service/ShopService.java
+++ src/main/java/org/knulikelion/moneyisinvest/service/ShopService.java
@@ -2,18 +2,21 @@ package org.knulikelion.moneyisinvest.service;
 
 import org.knulikelion.moneyisinvest.data.dto.response.BaseResponseDto;
 import org.knulikelion.moneyisinvest.data.dto.response.ShopHistoryResponseDto;
+import org.knulikelion.moneyisinvest.data.dto.response.ShopItemListResponseDto;
 import org.knulikelion.moneyisinvest.data.entity.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ShopService {
-    List<Shop> getAllItems();
+    Page<ShopItemListResponseDto> getAllItems(Pageable pageable);
 
     BaseResponseDto uploadShopItems(MultipartFile file, String itemName, String itemCategory, double stockPrice);
 
-    Optional<Shop> getItemsById(Long id);
+    ShopItemListResponseDto getItemsById(Long id);
     BaseResponseDto buyItemsById(Long id, String username);
 
     List<ShopHistoryResponseDto> getShopHistory(String username);
