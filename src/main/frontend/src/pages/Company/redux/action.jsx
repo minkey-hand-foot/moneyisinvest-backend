4efbca01264caf96d0cde9bc7f2307d3e55fe824
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-08-19 23:11:13 +0900

    fix: Remove React
    Docker 배포 예정..

--- src/main/frontend/src/pages/Company/redux/action.jsx
+++
@@ -1,9 +0,0 @@
-  export const updateStock = (data) => ({
-    type: 'UPDATE_STOCK_DATA',
-    payload: data,
-  });
-
-  export const storeStock = (data) => ({
-    type: 'STORE_STOCK_DATA',
-    payload: data,
-  });
