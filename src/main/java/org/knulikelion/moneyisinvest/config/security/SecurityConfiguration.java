527ab1384245c2194516e1f6dbd371b857504126
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-08-29 03:24:21 +0900

    feat: WebSecurity 인증 권한 변경

--- src/main/java/org/knulikelion/moneyisinvest/config/security/SecurityConfiguration.java
+++ src/main/java/org/knulikelion/moneyisinvest/config/security/SecurityConfiguration.java
@@ -93,6 +93,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity webSecurity) {
-        webSecurity.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/images/**");
+        webSecurity.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**");
     }
 }
