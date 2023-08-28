a43bf364da4a0d36c1c721b2552d746f860c7a44
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-29 01:38:07 +0900

    feat : JwtTokenProvider refresh 기능 추가

--- src/main/java/org/knulikelion/moneyisinvest/config/security/JwtTokenProvider.java
+++ src/main/java/org/knulikelion/moneyisinvest/config/security/JwtTokenProvider.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
@@ -17,9 +18,11 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -29,7 +32,34 @@ public class JwtTokenProvider {
 
     @Value("${springboot.jwt.secret}")
     private String secretKey = "secretKey";
-    private final long tokenValidMillisecond = 1000L * 60 * 60; // 1시간 토큰 유효
+    private final long accessTokenValidMillisecond = 1000L * 60 * 60; // 1시간 토큰 유효
+    private final long refreshTokenValidMillisecond = 1000L * 60 * 60 * 24 * 14; /*2주 토큰 유효*/
+
+    public String createAccessToken(String userEmail, List<String> roles){
+        return createToken(userEmail, roles, accessTokenValidMillisecond);
+    }
+
+    public String createRefreshToken(String userEmail){
+        return createToken(userEmail, new ArrayList<>(), refreshTokenValidMillisecond);
+    }
+
+    public String refreshToken(String refreshToken, String userEmail){
+        LOGGER.info("[refreshToken] 리프레시 토큰으로 액세스 토큰 재발급 시작");
+
+         /*리프레시 토큰 유효성 검사*/
+        if (validateToken(refreshToken)){
+            /*유저 이메일로 새로운 액세스 토큰 생성*/
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            List<String> roles = userDetails.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+
+            return createAccessToken(userEmail,roles);
+        }else{
+            LOGGER.error("[refreshToken] 리프레시 토큰이 유효하지 않음");
+            throw new RuntimeException("Refresh token is not valid");
+        }
+    }
 
     @PostConstruct
     protected void init() {
@@ -39,7 +69,7 @@ public class JwtTokenProvider {
     }
 
     // JWT 토큰 생성
-    public String createToken(String userUid, List<String> roles) {
+    public String createToken(String userUid, List<String> roles, long validMillisecond) {
         LOGGER.info("[createToken] 토큰 생성 시작");
         Claims claims = Jwts.claims().setSubject(userUid);
         claims.put("roles", roles);
@@ -51,7 +81,7 @@ public class JwtTokenProvider {
 //                토큰 발행 시간 정보
                 .setIssuedAt(now)
 //                토큰 만료 시간 정보
-                .setExpiration(new Date(now.getTime() + tokenValidMillisecond))
+                .setExpiration(new Date(now.getTime() + validMillisecond))
 //                암호화 알고리즘, secret 값 세팅
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
