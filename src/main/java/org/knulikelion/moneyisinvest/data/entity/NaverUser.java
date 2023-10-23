964fc7b5f4dc6401ae5659575cd868723a8e67b9
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-10-24 01:53:57 +0900

    feat: naver 로그인 엔티티 생성.

---
+++ src/main/java/org/knulikelion/moneyisinvest/data/entity/NaverUser.java
@@ -0,0 +1,11 @@
+package org.knulikelion.moneyisinvest.data.entity;
+
+
+import lombok.Data;
+
+@Data
+public class NaverUser {
+    private String profileImageUrl;
+    private String nickname;
+    private String email;
+}
